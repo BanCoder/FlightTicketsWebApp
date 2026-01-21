6cb2ab2229cce718f131eca8a04e34345100532b
Vladimir Belskiy <belskiy548@gmail.com>
2026-01-21 16:19:47 +0300

    Fix min value for flight and booking date

--- FlightTicketsWeb/obj/Debug/net8.0/FlightTicketsWeb.AssemblyInfo.cs
+++ FlightTicketsWeb/obj/Debug/net8.0/FlightTicketsWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlightTicketsWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad2fe4edb0d2914bb17ee1e0f50aeb18adc7ee8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d754129ce300e82e1313bc2b47c1e97f69f150a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlightTicketsWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlightTicketsWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
